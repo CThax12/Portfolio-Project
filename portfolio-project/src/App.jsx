306259ee0b3127ca439efcb1a8fa624753aae823
Cthax12 <Cthax12@gmail.com>
2025-09-22 22:06:26 -0500

    add loading bar base

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { LoadingScreen } from './components/LoadingScreen'
 
 function App() {
+    return (
+    <>
+    <LoadingScreen />
+    </>
+    )
 }
 export default App
