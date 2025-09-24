5b8c8289e1bd4ee408b5531d2bc1dd6798802745
Cthax12 <Cthax12@gmail.com>
2025-09-23 21:23:04 -0500

    add name and type writer effect

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,10 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import { LoadingScreen } from './components/LoadingScreen'
+import "./index.css"
 
 function App() {
+    const [isLoading, setIsLoading] = useState(false);
     return (
     <>
-    <LoadingScreen />
+    {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}
     </>
     )
 }
