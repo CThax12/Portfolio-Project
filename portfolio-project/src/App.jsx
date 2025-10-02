d4bad37951339096d1bf456c44217419c17fed39
Cthax12 <Cthax12@gmail.com>
2025-10-01 21:30:21 -0500

    add navbar

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { LoadingScreen } from './components/LoadingScreen'
+import { useState } from "react"
+import "./App.css"
+import { LoadingScreen } from "./components/LoadingScreen"
 import "./index.css"
+import { Navbar } from "./components/Navbar"
 
 function App() {
-    const [isLoading, setIsLoading] = useState(false);
-    return (
+  const [isLoading, setIsLoading] = useState(true)
+
+  return (
     <>
-    {!isLoading && <LoadingScreen onComplete={() => setIsLoading(true)} />}{""}
-        <div className={'min-h-screen transition-opacity duration-700 ${isLoading ? "opacity-100" : "opacity-0"} bg-black text-gray-100'}>
-            <Navbar />
-        </div>
+      {isLoading && <LoadingScreen onComplete={() => setIsLoading(false)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          isLoading ? "opacity-0" : "opacity-100"
+        } bg-black text-gray-100`}
+      >
+        <Navbar />
+      </div>
     </>
-    )
+  )
 }
+
 export default App
