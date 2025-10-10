7dfc63dd57ad8d658843130717a501d08810a940
Cthax12 <Cthax12@gmail.com>
2025-10-10 01:20:53 -0500

    added about me section

---
+++ portfolio-project/src/components/sections/About.jsx
@@ -0,0 +1,86 @@
+export const About = () => {
+  const techStack = ["C#/.NET", "PHP", "Java", "SQL", "Python"];
+
+  const skills = ["REST APIs", "WPF", "Linux", "Git", "Docker"];
+
+  return (
+    <section
+      id="about"
+      className="flex  min-h-screen items-center justify-center  py-20"
+    >
+      <div className="max-w-5xl mx-auto px-4">
+        <h2
+          className="text-5xl font-bold mb-8
+                bg-gradient-to-r from-blue-500 to-cyan-400 bg-clip-text text-transparent text-center"
+        >
+          About Me
+        </h2>
+
+        <div className=" rounded-2xl p-8 border-white/10 border hover:-translate-y-1 transition-all ">
+          <p className="text-gray-300 mb-6">Put some info about me.</p>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Tech Stack</h3>
+              <div className=" items-center justify-center flex flex-wrap gap-2">
+                {techStack.map((tech, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                  >
+                    {tech}
+                  </span>
+                ))}
+              </div>
+            </div>
+
+            <div className="rounded-xl p-6 hover:-translate-y-1 transition-all">
+              <h3 className="text-xl font-bold mb-4">Skills / Tech</h3>
+              <div className="flex flex-wrap gap-2 items-center justify-center">
+                {skills.map((skill, key) => (
+                  <span
+                    key={key}
+                    className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,246,0.2)] transition"
+                  >
+                    {skill}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6 mt-8">
+          <div className="p-6 rounded-xl  border-white/10 border hover:-translate-y-1 transition-all ">
+            <h3 className="text-xl font-bold mb-4 ">🎓 Education</h3>
+            <ul className=" list-inside text-gray-300 space-y-2">
+              <li>
+                <strong>B.S. Computer Science</strong> - Southwestern Oklahoma
+                Statue University (2023) 4.0 GPA
+              </li>
+            </ul>
+          </div>
+          <div className="p-6 rounded-xl  border-white/10 border hover:-translate-y-1 transition-all ">
+            <h3 className="text-xl font-bold mb-4 ">💼 Work Experience</h3>
+            <div className="space-y-4 text-gray-300">
+              <div className="">
+                <h4 className="font-semibold">
+                  Software Engineer at Long Wave Inc. (2025-Present)
+                </h4>
+                <p>Stuff I did</p>
+                <h4 className="font-semibold">
+                  Software Engineer at Paycom (2023-2025)
+                </h4>
+                <p>Stuff I did</p>
+                <h4 className="font-semibold">
+                  Software Engineer at Cantina Capital LLC (2020-P2022)
+                </h4>
+                <p>Stuff I did</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
--- portfolio-project/src/components/sections/Home.jsx
+++ portfolio-project/src/components/sections/Home.jsx
@@ -1,8 +1,43 @@
 export const Home = () => {
-    return <section id="home" className="min-h-screen flex items-center justify-center relative">
-        <div className="text-center z-10 px-4">
-            <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-400 to bg-purple-600 bg-clip-text leading-right text-transparent">Hey, I'm Connor Thaxton</h1>
-        </div>
+  return (
+    <section
+      id="home"
+      className="min-h-screen flex items-center justify-center relative"
+    >
+      <div className="text-center z-10 px-4">
+        <h1 className="text-4xl md:text-6xl font-bold mb-6 bg-gradient-to-r  from-blue-500 to-cyan-400 bg-clip-text leading-right text-transparent">
+          Hey, I'm Connor Thaxton
+        </h1>
 
+        <p className="text-gray-400 text-m mb-8 max-w-lg mx-auto">
+          I’m a software engineer who’s worked across a mix of tech stacks and
+          loves tackling tough problems. I’m big on helping people and building
+          things that actually make life easier. As a former college athlete, I
+          love teamwork and new challenges. My experience has given me the
+          chance to work directly with clients and find solutions that really
+          make a difference.
+        </p>
+
+        <div className="flex justify-center space-x-4">
+          <a
+            href="#projects"
+            className="bg-blue-500 text-white py-3 px-6 rounded font-medium transition relative overflow-hidden 
+            hover:-translate-y-0.5 hover:shadow[0,0,15px_rgba(59,130,256,0.4)]"
+          >
+            View Projects
+          </a>
+
+          <a
+            href="#contact"
+            className="border border-blue-500/50
+             text-blue-500 py-3 px-6 font-medium transition-all duration-200
+              overflow-hidden 
+            hover:-translate-y-0.5 hover:shadow[0,0,15px_rgba(59,130,256,0.2)] hover:bg-blue-500"
+          >
+            Contact Me
+          </a>
+        </div>
+      </div>
     </section>
-}
+  );
+};
